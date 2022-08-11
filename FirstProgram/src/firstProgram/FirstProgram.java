f2940e636098216b0a287f05c71c4a4925aef6d5
Konstantine Savrasov <ksavrasov@gmail.com>
2022-08-11 03:00:48 +0300

    first demo commit

---
+++ FirstProgram/src/firstProgram/FirstProgram.java
@@ -0,0 +1,8 @@
+package firstProgram;
+
+public class FirstProgram { 
+	   public static void main(String []args) {
+	      /* println() function to write Welcome to the basics of programming! */
+	      System.out.println("Welcome to the basics of programming!");     
+	   }
+	}
